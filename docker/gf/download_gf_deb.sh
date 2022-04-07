82b1fce360c1ae1115a79102d866b98102f9e768
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2022-04-07 09:35:19 -0400

    curl pour télécharger (fonctionne en Windows avec GitBash)

--- docker/gf/download_gf_deb.sh
+++ docker/gf/download_gf_deb.sh
@@ -1,1 +1,1 @@
-wget https://github.com/GrammaticalFramework/gf-core/releases/download/3.11/gf-3.11-ubuntu-18.04.deb
+curl https://github.com/GrammaticalFramework/gf-core/releases/download/3.11/gf-3.11-ubuntu-18.04.deb --output gf-3.11-ubuntu-18.04.deb
