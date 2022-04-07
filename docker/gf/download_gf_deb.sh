29297d8f86752b7ce6ec09913d6dcfe640478621
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2022-04-07 09:38:55 -0400

    curl -L pour suivre la redirection

--- docker/gf/download_gf_deb.sh
+++ docker/gf/download_gf_deb.sh
@@ -1,1 +1,2 @@
-curl https://github.com/GrammaticalFramework/gf-core/releases/download/3.11/gf-3.11-ubuntu-18.04.deb --output gf-3.11-ubuntu-18.04.deb
+curl https://github.com/GrammaticalFramework/gf-core/releases/download/3.11/gf-3.11-ubuntu-18.04.deb 
+curl -L https://github.com/GrammaticalFramework/gf-core/releases/download/3.11/gf-3.11-ubuntu-18.04.deb --output gf-3.11-ubuntu-18.04.deb
